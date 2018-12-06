e47e284adf472209c0b1ca635bb1401ebcdfc882
Milton Eiji Takamura <miltontakamura@gmail.com>
2018-12-05 22:47:30 -0200

    Add stub HomeScreen

---
+++ webapp/src/screens/HomeScreen/Home.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Home = () => <div>Home</div>
+
+export default Home;
---
+++ webapp/src/screens/HomeScreen/HomeScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Home from './Home';
+
+class HomeScreen extends React.Component {
+
+  render() {
+    return (
+      <Home />
+    );
+  }
+}
+
+export default HomeScreen;
