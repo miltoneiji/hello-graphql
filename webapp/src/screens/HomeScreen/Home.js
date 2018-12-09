ef98beba600e77581911a7324995e608073f3def
Milton Eiji Takamura <miltontakamura@gmail.com>
2018-12-09 17:15:15 -0200

    Use QueryRenderer on HomeScreen

--- webapp/src/screens/HomeScreen/Home.js
+++ webapp/src/screens/HomeScreen/Home.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
+import { createFragmentContainer, graphql } from 'react-relay';
 
-const Home = () => <div>Home</div>
+const Home = ({ user }) => (
+  <div>
+    { user ? `Hello ${user.name}` : 'Unknown user' }
+  </div>
+);
 
-export default Home;
+export default createFragmentContainer(
+  Home,
+  graphql`
+    fragment Home_user on User {
+      name
+    }
+  `
+);
--- webapp/src/screens/HomeScreen/HomeScreen.js
+++ webapp/src/screens/HomeScreen/HomeScreen.js
@@ -1,13 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { graphql, QueryRenderer } from 'react-relay';
 
+import environment from '../../Environment';
 import Home from './Home';
 
 class HomeScreen extends React.Component {
 
   render() {
+    const query = graphql`
+      query HomeScreenQuery {
+        me {
+          ...Home_user
+        }
+      }
+    `;
+
     return (
-      <Home />
+      <QueryRenderer
+        environment={environment}
+        query={query}
+        render={({ error, props }) => {
+          if (error) {
+            return <div>Error</div>;
+          }
+          else if(!props) {
+            return <div>Loading</div>;
+          }
+          else {
+            return <Home user={props.me} />;
+          }
+        }}
+      />
     );
   }
 }
