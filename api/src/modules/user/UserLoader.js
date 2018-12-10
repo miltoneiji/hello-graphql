ad59e9859f080bd502ee93ba337ee7dfc982fcb9
Milton Eiji Takamura <miltontakamura@gmail.com>
2018-12-10 07:52:58 -0200

    Model should not be used as a DTO

--- api/src/modules/user/UserLoader.js
+++ api/src/modules/user/UserLoader.js
@@ -1,21 +1,21 @@
 import UserModel from './UserModel';
 
-export const load = async (context, id) => {
-  if (!id) {
+export const load = async (context, _id) => {
+  if (!_id) {
     return null;
   }
 
   let data;
   try {
-    data = await context.database.userDataById(id);
+    data = await context.database.userDataById(_id);
   } catch(err) {
     console.log(err);
     return null;
   }
 
-  return new UserModel({
+  return {
     _id: data._id,
     name: data.name,
     email: data.email,
-  });
+  };
 };
