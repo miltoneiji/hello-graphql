4f099eecb094b07eab2930684ad7332560282d5f
Milton Eiji Takamura <miltontakamura@gmail.com>
2018-12-10 07:53:35 -0200

    Fix bugs related to database/index.js

--- api/src/database/index.js
+++ api/src/database/index.js
@@ -11,14 +11,10 @@ const inMemoryDatabase = {
 };
 const createId = () => crypto.randomBytes(10).toString('hex');
 const calcHash = word => crypto.createHash('sha256').digest(word).toString('hex');
-
-const UserNotFound = (userId) => new Error(`User with id ${userId} not found.`);
+const debug = () => console.log(inMemoryDatabase);
 
 const userDataById = async userId => {
   const userData = inMemoryDatabase.users[userId];
-  if (!userData) {
-    return UserNotFound(userId);
-  }
   return userData;
 };
 
@@ -26,29 +22,30 @@ const insertUserData = async ({ name, email, password }) => {
   const _id = createId();
   const passwordHash = calcHash(password);
 
-  inMemoryDatabase[_id] = {
+  inMemoryDatabase.users[_id] = {
     _id,
     name,
     email,
     passwordHash,
   };
 
-  return inMemoryDatabase[_id];
+  return inMemoryDatabase.users[_id];
 };
 
 const userDataByEmail = async email => {
-  const userData = Object.values(inMemoryDatabase).find(data => data.email === email);
+  const userData = Object.values(inMemoryDatabase.users).find(data => data.email === email);
   return userData;
 };
 
 const userDataByEmailAndPassword = async ({ email, password }) => {
   const passwordHash = calcHash(password);
-  return Object.values(inMemoryDatabase).find(
+  return Object.values(inMemoryDatabase.users).find(
     data => data.email === email && data.passwordHash === passwordHash
   );
 };
 
 export default {
+  debug,
   userDataById,
   userDataByEmail,
   insertUserData,
