1261fe17f3106ac72810fc6762f4baef17e1a14f
Milton Eiji Takamura <miltontakamura@gmail.com>
2018-12-12 20:49:38 -0200

    [api] Webpack and typescript setup

---
+++ api/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const config = require('./src/config');
+
+module.exports = {
+  mode: config.env,
+  target: 'node',
+  entry: './src/server.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'server.js',
+  },
+  resolve: {
+    extensions: ['.mjs', '.js', '.ts'],
+  },
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        test: /\.(ts|js)$/,
+        exclude: /node_modules/,
+      }
+    ]
+  }
+};
